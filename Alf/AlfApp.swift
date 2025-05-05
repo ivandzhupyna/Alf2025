7119ab5903cfbd2e2e7056fd28e9f3b35623a736
Ivan <ivandzhupyna@gmail.com>
2025-05-05 22:26:04 +0200

    Initial Commit

---
+++ Alf/AlfApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlfApp.swift
+//  Alf
+//
+//  Created by Ivan Dzhupyna on 05.05.25.
+//
+
+import SwiftUI
+
+@main
+struct AlfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlfTests/AlfTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlfTests.swift
+//  AlfTests
+//
+//  Created by Ivan Dzhupyna on 05.05.25.
+//
+
+import Testing
+@testable import Alf
+
+struct AlfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
